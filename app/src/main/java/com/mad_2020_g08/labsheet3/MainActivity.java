1aa5504230496c3f603aee0d903b89696a7b64bf
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2020-07-30 13:02:13 +0530

    Initial commit

---
+++ app/src/main/java/com/mad_2020_g08/labsheet3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mad_2020_g08.labsheet3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
